59abe2fe0df7e8c23eda1cdd8430c4b95c1c8dda
vavillco <116744360+vavillco@users.noreply.github.com>
2024-09-14 19:22:01 -0300

    inversa

--- funciones.py
+++ funciones.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.linalg import solve_triangular
 
 def calcularLU(A):
     L, U = [],[]
@@ -12,6 +13,17 @@ def calcularLU(A):
 def inversaLU(L, U, P=None):
     Inv = []
     # su código
-    
+    n = L.shape[0]
+    M_identidad = []
+    Inv = np.zeros((n, 0)) 
+    for i in range(n):
+        canonico = np.zeros(n)
+        canonico[i] = 1
+        M_identidad.append(canonico)
+    for i in range(n):
+        x = solve_triangular(L, M_identidad[i], lower=True)
+        y = solve_triangular(U, x, lower=False)
+        Inv = np.column_stack((Inv, y))  
+        # capaz que no se pueda usar esta funcion, preguntar
     ###########
     return Inv
