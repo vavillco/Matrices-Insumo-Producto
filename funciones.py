2222e95a644301d889356a65f97c066d587dcd6d
NuviPvP <cristiantepe4589@gmail.com>
2024-09-25 15:11:47 -0300

    ultimos retoques v1.1

--- funciones.py
+++ funciones.py
@@ -1,10 +1,24 @@
 import numpy as np
 from scipy.linalg import solve_triangular
 
+import numpy as np
+from scipy.linalg import solve_triangular
+
 def calcularLU(A):
+    """
+    Calcular la factorización LU de una matriz.
+
+    Args:
+        A (numpy.ndarray): Matriz cuadrada que se desea factorizar.
+
+    Returns:
+        L (numpy.ndarray): Matriz triangular inferior L.
+        U (numpy.ndarray): Matriz triangular superior U.
+        P (numpy.ndarray): Matriz de permutación P.
+    """
     m = A.shape[0]
     n = A.shape[1]
-    c = A.copy()
+    Ac = A.copy()
     P = np.eye(n)
     
     if m != n:
@@ -16,13 +30,13 @@ def calcularLU(A):
     for i in range(0, n):   
         max_row = np.argmax(np.abs(Ac[i:n, i])) + i
         if i != max_row:
-            # Intercambia las filas en Ac
+            # Intercambiamos las filas en Ac
             Ac[[i, max_row], :] = Ac[[max_row, i], :]
             
-            # Intercambia las filas en P
+            # Intercambiamos las filas en P
             P[[i, max_row], :] = P[[max_row, i], :]
             
-            # Intercambia las filas en L hasta la columna i (excluyendo la diagonal)
+            # Intercambiamos las filas en L hasta la columna i (excluyendo la diagonal)
             if i > 0:
                 L[[i, max_row], :i] = L[[max_row, i], :i]
             
@@ -36,18 +50,26 @@ def calcularLU(A):
 
 
 def inversaLU(L, U, P):
+    """
+    Calculo de la inversa de una matriz a partir de su factorización LU.
+
+    Args:
+        L (numpy.ndarray): Matriz triangular inferior L.
+        U (numpy.ndarray): Matriz triangular superior U.
+        P (numpy.ndarray): Matriz de permutación P.
+
+    Returns:
+        numpy.ndarray: Matriz inversa de la matriz original A que fue factorizada en L, U y P.
+    """
     n = L.shape[0]
-    M_identidad = []
     Inv = np.zeros((n, 0)) 
 
     if P is None:
-        P = np.eye(n) 
-
-    # Resolver el sistema para cada columna de la identidad permutada
+        P = np.eye(n)  # Si no se proporciona P, se usa la identidad
     for i in range(n):
-        b = P[:, i]  # Columna i de la matriz de permutación
+        b = P[:, i]  
         x = solve_triangular(L, b, lower=True)
         y = solve_triangular(U, x, lower=False)
-        Inv = np.column_stack((Inv, y))  # Agregar la columna de la inversa
+        Inv = np.column_stack((Inv, y))  # Agregamos la columna de la inversa
 
     return Inv
